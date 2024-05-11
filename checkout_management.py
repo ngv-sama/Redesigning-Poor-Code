1c16cc685419462d20da5f8ef0a504156f167f80
ngv <nikhil.guru.v@gmail.com>
2024-05-11 18:22:06 +0530

    Changes

--- checkout_management.py
+++ checkout_management.py
@@ -2,26 +2,22 @@ from storage import Session, init_db
 from models import Users, Books, Checkout, Admins
 
 def checkout_book():
-    session=Session()
-    user_id=input("Enter your User Id: ")
-    book_id=input("Enter the Book Id: ")
-    user_id = session.query(Users).get(user_id)
-    book_id=session.query(Books).get(book_id)
-    if(user_id==None or book_id==None):
-        print("User not found, please contact Admin")
+    session = Session()
+    user_id = int(input("Enter your User Id: "))
+    book_id = int(input("Enter the Book Id: "))
+    user = session.query(Users).get(user_id)
+    book = session.query(Books).get(book_id)
+
+    if user is None or book is None:
+        print("User or book not found, please contact Admin")
         session.close()
         return
-    
-    book = session.query(Books).filter_by(id=book_id).one_or_none()
-    if book is None:
-        print("No such book exists.")
-        return
+
     if not book.availability:
         print("This book is currently unavailable.")
         return
 
-    
-    new_checkout = Checkout(user_id=user_id, book_id=book_id)
+    new_checkout = Checkout(user_id=user.id, book_id=book.id)
     session.add(new_checkout)
 
     book.availability = False
@@ -33,7 +29,7 @@ def checkout_book():
 
 def check_in():
     session=Session()
-    book_id = input("Enter the Book Id: ")
+    book_id = int(input("Enter the Book Id: "))
     book = session.query(Books).filter_by(id=book_id).one_or_none()
     if book is None:
         print("No such book exists.")
